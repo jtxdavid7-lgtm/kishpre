37af5e2214ad0d51299cd9dd032521c405f90d4c
openkish <openkish@users.noreply.github.com>
2026-04-05 23:16:18 +0800

    deploy: reorder equity layout

--- src/data/base/index.js
+++
@@ -1,9 +0,0 @@
-import { BTN_open_100bb } from './BTN_open_100bb';
-
-export const BASE_RANGES = {
-  [BTN_open_100bb.key]: BTN_open_100bb
-};
-
-export function getBaseRange(sceneKey) {
-  return BASE_RANGES[sceneKey];
-}
